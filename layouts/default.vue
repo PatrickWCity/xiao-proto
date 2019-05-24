c7e47703636be01324b15a0fae2e3394148c3f75
Patrick Ciudad <PatrickWCity@gmail.com>
2019-05-24 03:50:57 -0400

    Clean default pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,41 @@
 <template>
   <div>
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-container>
+        <nuxt-link class="navbar-brand" to="/">XIAO</nuxt-link>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/empleos" active-class="active">
+                Empleos
+              </nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/vehiculos" active-class="active">
+                Vehículos
+              </nuxt-link>
+            </li>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/checkout" active-class="active">
+                Checkout
+              </nuxt-link>
+            </li>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
     <nuxt />
   </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+body {
+  background-color: #f8f9fa;
 }
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.navbar-dark .navbar-nav .nav-link {
+  color: rgba(255, 255, 255, 0.55);
 }
 </style>
