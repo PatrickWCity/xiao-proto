fed7ce7ddd820975e4455e17f7868570fea31a3f
Patrick Ciudad <PatrickWCity@gmail.com>
2019-06-08 16:52:01 -0400

    limpieza inicial

--- pages/categorias/index.vue
+++ pages/categorias/index.vue
@@ -26,25 +26,6 @@
               </div>
             </div>
           </div>
-          <div class="col-12">
-            <nav aria-label="...">
-              <ul class="pagination">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item active">
-                  <a class="page-link" href="#"
-                    >2 <span class="sr-only">(current)</span></a
-                  >
-                </li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">Next</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
         </div>
       </div>
     </div>
