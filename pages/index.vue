3a788104b6eef04086cd3d405642c51f53c6b915
Patrick Ciudad <PatrickWCity@gmail.com>
2019-06-09 23:01:46 -0400

    inicial

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,12 @@
   <section class="jumbotron text-center">
     <div class="container">
       <h1 class="jumbotron-heading text-uppercase">
-        Derniers Résultats EuroMillions
+        Avisos de Clasificados en Chile
       </h1>
-
-      <p>Vendredi 02 février 2018</p>
-
-      <p>
-        <span class="btn btn-lg btn-dark rounded">14</span>
-        <span class="btn btn-lg btn-dark rounded">34</span>
-        <span class="btn btn-lg btn-dark rounded">36</span>
-        <span class="btn btn-lg btn-dark rounded">39</span>
-        <span class="btn btn-lg btn-dark rounded">48</span>
-        <span class="btn btn-lg btn-warning rounded">2</span>
-        <span class="btn btn-lg btn-warning rounded">3</span>
-      </p>
-
+      <p>Vehículos</p>
       <h3>
-        <small> MY MYLLION</small>
+        <small>¡Bienvenidos!</small>
       </h3>
-      <h3><span class="badge badge-dark">WB 404 9390</span></h3>
     </div>
   </section>
 </template>
